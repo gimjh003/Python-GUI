5464f8b81b9dc8871a423e5136680dee1c45b0e9
gimjh003 <46261152+gimjh003@users.noreply.github.com>
2022-04-09 00:25:56 +0900

    .

--- GUI_basic/15_quiz.py
+++ GUI_basic/15_quiz.py
@@ -1,37 +1,39 @@
+import os
 from tkinter import *
 root = Tk()
 root.title("제목 없음 - Windows 메모장")
-root.geometry("640x480")
-
-# 본문
+root.geometry('640x480')
+filename = "mynote.txt"
 
 scrollbar = Scrollbar(root)
 scrollbar.pack(side="right", fill="y")
-txt = Text(root, width=1920, yscrollcommand=scrollbar.set)
-txt.pack(side="left", fill="both")
+txt = Text(root, yscrollcommand=scrollbar.set)
+txt.pack(side="left", fill="both", expand=True)
 scrollbar.config(command=txt.yview)
 
-# 동작
+def open_file():
+    if os.path.isfile(filename):
+        with open("mynote.txt", "r", encoding="utf8") as file:
+            txt.delete("1.0", END)
+            txt.insert(END, file.read())
 
-def fopen():
-    with open("mynote.txt", "r") as file:
-        txt.delete("1.0", END)
-        txt.insert(END, file.read())
-
-def save():
-    with open("mynote.txt", "w", encoding="utf8") as file:
-        file.write(txt.get("1.0", END))
-
-# 메뉴
+def save_file():
+    with open("mynote.txt", 'w', encoding='utf8') as file:
+        file.write(txt.get('1.0', END))
 
 menu = Menu(root, tearoff=0)
 
 menu_file = Menu(menu, tearoff=0)
-menu_file.add_command(label="열기", command=fopen)
-menu_file.add_command(label="저장", command=save)
+menu_file.add_command(label="열기", command=open_file)
+menu_file.add_command(label="저장", command=save_file)
 menu_file.add_separator()
 menu_file.add_command(label="종료", command=root.quit)
+
 menu.add_cascade(label="파일", menu=menu_file)
+menu.add_cascade(label="편집")
+menu.add_cascade(label="서식")
+menu.add_cascade(label="보기")
+menu.add_cascade(label="도움말")
 root.config(menu=menu)
 
 root.mainloop()
